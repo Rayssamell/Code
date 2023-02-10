3a0cf7615f79a9f74393fb6162c01319d3efea63
Rayssa Mell <rayssamelltpe@gmail.com>
2023-02-10 20:21:45 -0300

    adicionando app usuario

--- projeto/urls.py
+++ projeto/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
-    path('forum/', include('forum.urls')),
+    # path('forum/', include('forum.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
